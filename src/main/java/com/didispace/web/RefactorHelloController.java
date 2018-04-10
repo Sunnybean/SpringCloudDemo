c777fab0f9acc5eee2d96968cf9a69f06bf89daa
hailong5214 <418338936@qq.com>
2018-04-11 00:10:25 +0800

    s's

---
+++ src/main/java/com/didispace/web/RefactorHelloController.java
@@ -0,0 +1,24 @@
+package com.didispace.web;
+
+
+import org.springframework.web.bind.annotation.*;
+
+//@RestController
+//public class RefactorHelloController implements HelloService {
+//
+//	@Override
+//	public String hello(@RequestParam("name") String name) {
+//		return "Hello " + name;
+//	}
+//
+//	@Override
+//	public User hello(@RequestHeader("name") String name, @RequestHeader("age") Integer age) {
+//		return new User(name, age);
+//	}
+//
+//	@Override
+//	public String hello(@RequestBody User user) {
+//		return "Hello "+ user.getName() + ", " + user.getAge();
+//	}
+//
+//}
